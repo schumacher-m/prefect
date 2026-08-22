d34cdf0d1c929fb9ddd6e618a6935289f7e8d60e
Michael <schumacher.m@me.com>
2026-08-22 11:21:35 +0200

    feat(ui): add saved deployment filter presets

--- ui-v2/src/components/deployments/data-table/index.tsx
+++ ui-v2/src/components/deployments/data-table/index.tsx
@@ -36,6 +36,7 @@ import { TagsInput } from "@/components/ui/tags-input";
 import { useLocalStorage } from "@/hooks/use-local-storage";
 import { createColumnHelper, useTable } from "@/lib/tanstack-table";
 import { pluralize } from "@/utils";
+import { DeploymentsFilterPresetsBar } from "../deployments-filter-presets-bar";
 import { ActionsCell, ActivityCell } from "./cells";
 
 export type DeploymentsDataTableProps = {
@@ -285,6 +286,15 @@ export const DeploymentsDataTable = ({
 					</Select>
 				</div>
 			</div>
+
+			<DeploymentsFilterPresetsBar
+				columnFilters={columnFilters}
+				sort={sort}
+				onColumnFiltersChange={onColumnFiltersChange}
+				onSortChange={onSortChange}
+				onClearFilters={onClearFilters}
+				className="pb-3"
+			/>
 
 			<DeleteConfirmationDialog {...deleteConfirmationDialogState} />
 			{showFilteredEmptyState ? (
